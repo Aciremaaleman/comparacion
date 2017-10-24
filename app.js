ae9963f3ea132716948c0e320a61e19936fdd87e
aciremaaleman <aciremaaleman@outlook.es>
2017-10-24 15:16:15 -0500

    Suebiendo las actualizaciones de la carpeta comparaciones

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var comparacion = function(array1,array2){
-  document.write('declarado');
+  if(array1.length !== array2.length){
+    return false;
+  }else{
+    for(var i=0; i<arrayi.length; i++) {
+      if(array1[i] !== array2[i]){
+        return false
+      }
+    }
+  }
 }
 
 var arreglo1 = [1,2,3,4,5,6,7,8,9,10];
